430e950830c95681b39eb4a51aca66d6d5afdc31
JPA-EliteDeveloper <algadipej962@gmail.com>
2023-02-20 12:16:27 +0800

    save

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => '/gpay.com/login/auth/callback',
+        'redirect' => 'https://example2-self.vercel.app/gpay.com/login/auth/callback',
     ],
     //
 
