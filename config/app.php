6e345a0f3b3e242bf74d7f674146fccaa699a1ca
JPA-EliteDeveloper <algadipej962@gmail.com>
2023-02-21 16:26:41 +0800

    new configuration

--- config/app.php
+++ config/app.php
@@ -181,7 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider::class,
+        // CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider::class,
 
 
         /*
@@ -212,7 +212,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
-        'Cloudinary' => CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary::class,
+        // 'Cloudinary' => CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary::class,
     ])->toArray(),
 
 ];
